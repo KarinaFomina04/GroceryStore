26f8ad133c044dfec85d721d358fa842ded100bd
KarinaFomina04 <karinafomina.19970804@gmail.com>
2021-06-01 00:43:45 +0300

    Added start and category

--- bot.py
+++ bot.py
@@ -7,9 +7,27 @@ config.read("settings.ini")
 bot = telebot.TeleBot(config["TELEGRAM"]["TG_TOKEN"])
 
 
-@bot.message_handler(content_types=['text'])
-def welcome(message):
-    bot.send_message(message.chat.id, message.text)
+def toString(collection):
+    output = ''
+
+    for i in range(len(collection)):
+        output = output + str(i+1) + '. ' + collection[i] + '\n'
+
+    return output
+
+
+categories = ['бакалея', 'десерты', 'напитки', 'чай']
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Добро пожаловать в {0.first_name}!\n Введите /category для вывода категорий "
+                                      "товаров.".format(bot.get_me()))
+
+
+@bot.message_handler(commands=['category'])
+def getCategory(message):
+    bot.send_message(message.chat.id, "Категории товаров:\n{0}".format(toString(categories)))
 
 
 bot.polling(none_stop=True)
